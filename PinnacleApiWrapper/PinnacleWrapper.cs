6a434c1968ffe0a7309b535b63c7622826129ef5
bigmat <13409135+bigmat@users.noreply.github.com>
2018-03-02 11:03:52 +0100

    Add constructor with login/password
    Add test on FixturesResponse and OddsResponse

--- PinnacleApiWrapper/PinnacleWrapper.cs
+++ PinnacleApiWrapper/PinnacleWrapper.cs
@@ -1,9 +1,19 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace PinnacleApiWrapper
 {
     public partial class PinnacleWrapper
     {
+        public PinnacleWrapper(string clientId, string password) : this(new HttpClient())
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{clientId}:{password}")));
+        }
+
         partial void UpdateJsonSerializerSettings(JsonSerializerSettings settings)
         {
             settings.Converters.Add(new GetBetsByTypeResponseJsonConverter());
